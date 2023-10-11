ac6ad899eddfc369f5362cd89cf8d87c8e2c610c
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-11 20:49:54 +0500

    update

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -11,7 +11,7 @@ import Input from "@/components/Input";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-
+  // some changes
   if (session) {
     return {
       redirect: {
@@ -67,7 +67,7 @@ const auth = () => {
     <div className="relative h-full w-full bg-[url('../public/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5 sticky top-0">
-          <h1 className="h-12 text-red-500 font-bold text-3xl" >Netflix</h1>
+          <h1 className="h-12 text-red-500 font-bold text-3xl">Netflix</h1>
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
